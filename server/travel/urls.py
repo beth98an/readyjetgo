99b6efed4eb4b30641c1535be0a7731621244747
anishakdeol <97612531+anishakdeol@users.noreply.github.com>
2022-05-20 11:59:14 +0100

    restframework working

--- server/travel/urls.py
+++ server/travel/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path 
-from . import views
+# from . import views 
+from .views import TravelList, TravelInd
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('', views.index, name='travel-index')
+    path('', TravelList.as_view()),
+    path('<int:id>', TravelInd.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
