a42089bdc0c9be4d31074a58052921576b0b9e9f
anishakdeol <97612531+anishakdeol@users.noreply.github.com>
2022-05-22 13:57:45 +0100

    updates

--- client/src/layout/NavBar/index.js
+++ client/src/layout/NavBar/index.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 function index() {
   return (
     <nav className="navbar">
-      <NavLink to='/'>Home</NavLink>
+      <NavLink to='/home'>Home</NavLink>
       <NavLink classname="register" to='/main'>Main</NavLink>
       <NavLink to='/login'>Log out</NavLink>
     </nav>
