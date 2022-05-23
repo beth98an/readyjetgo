84908c9a2e8bc4c2831c74cec4ae9f960fa07809
anishakdeol <97612531+anishakdeol@users.noreply.github.com>
2022-05-23 18:21:30 +0100

    styling for register

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -59,7 +59,8 @@ function Login() {
         <h1 id="title">Sign In</h1>
         <input type='text' value="Username"id="username" onChange={(e) => setUsername(e.target.value)}></input>
         <input type='password'value="Enter Password" id='password' onChange={(e) => setPassword(e.target.value)}></input>
-        <a id="forgot" href="#">Forgot Your Password?</a>
+        <a class="forgot" href="#">Forgot Your Password?</a>
+        <a class="forgot" href="/register">Not a member?  Create Account Now</a>
         <input type='submit' id="loginButton" value='Sign in'></input>
         
       </form>
--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -28,20 +28,26 @@ function Register() {
     return (
         <>
             <Header />
-            <div>
+            <div className="formdiv">
                 <form className="registerForm" onSubmit={handleSubmit}>
-                    <label htmlFor='username'>Enter Username: </label>
-                    <input type='text' id="username" onChange={(e) => setUsername(e.target.value)}></input>
-                    <label htmlFor='password'>Enter Password: </label>
-                    <input type='password' id='password' onChange={(e) => setPassword(e.target.value)}></input>
-                    <label htmlFor='confirm_password'>Confirm Password: </label>
-                    <input type='password' id='confirm_password' onChange={(e) => setConfirmPass(e.target.value)}></input>
-                    <input type='submit' value='Sign up'></input>
-                </form>
-            </div>
-            <p className='return-to-login' onClick={() => navigate('/login')}>
+                <h1 id="title">Register</h1>
+                    <input type='text' value='Enter username' id="username" onChange={(e) => setUsername(e.target.value)}></input>
+                    <input type='password' value='Enter password' id='password' onChange={(e) => setPassword(e.target.value)}></input>
+                    <input type='password' value='Confirm password' id='confirm_password' onChange={(e) => setConfirmPass(e.target.value)}></input>
+                    <input type='submit' id="signupButton" value='Sign up'></input>
+                    <a className='return-to-login' href="/login">
                 Made an account already? Click here to login.
-            </p>
+            </a>
+                </form>
+                <div className="box">
+        </div>
+            
+            
+            <div className="box2">
+            <h2>"The only bug you want is the travel bug"</h2>
+            <img src="https://cdn-icons-png.flaticon.com/512/201/201623.png"></img>
+            </div>
+            </div>
         </>
     )
 }
