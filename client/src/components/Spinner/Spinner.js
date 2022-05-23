72c01b88bcbd4595e136e528149fd684425d7ff3
FarzanaRU00 <Farzana29@hotmail.co.uk>
2022-05-23 11:08:27 +0100

    spinner

---
+++ client/src/components/Spinner/Spinner.js
@@ -0,0 +1,78 @@
+import React from "react";
+import 'styles.css'
+
+class App extends React.Component {
+
+    render() {
+        return(
+            <div>
+            <div className="arrow"></div>
+                <ul className="circle">
+                    <li>
+                        <div className="text"
+                        contenteditable="true"
+                        spellcheck="false">1</div>
+                    </li>
+                    <li>
+                        <div className="text"
+                        contenteditable="true"
+                        spellcheck="false">2</div>
+                    </li>
+                    <li>
+                        <div className="text"
+                        contenteditable="true"
+                        spellcheck="false">3</div>
+                    </li>
+                    <li>
+                        <div className="text"
+                        contenteditable="true"
+                        spellcheck="false">4</div>
+                    </li>
+                    <li>
+                        <div className="text"
+                        contenteditable="true"
+                        spellcheck="false">5</div>
+                    </li>
+                    <li>
+                        <div className="text"
+                        contenteditable="true"
+                        spellcheck="false">6</div>
+                    </li>
+                    <li>
+                        <div className="text"
+                        contenteditable="true"
+                        spellcheck="false">7</div>
+                    </li>
+                    <li>
+                        <div className="text"
+                        contenteditable="true"
+                        spellcheck="false">8</div>
+                    </li>
+                    <li>
+                        <div className="text"
+                        contenteditable="true"
+                        spellcheck="false">9</div>
+                    </li>
+                    <li>
+                        <div className="text"
+                        contenteditable="true"
+                        spellcheck="false">10</div>
+                    </li>
+                    <li>
+                        <div className="text"
+                        contenteditable="true"
+                        spellcheck="false">11</div>
+                    </li>
+                    <li>
+                        <div className="text"
+                        contenteditable="true"
+                        spellcheck="false">12</div>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+
+}
+
+export default App;
