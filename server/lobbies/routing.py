d140a0b9d8cb535c2efc4887a6bd22e7d13e3f65
Peter Ly <peter.ly1@hotmail.com>
2022-05-23 15:24:27 +0100

    websockets

--- server/lobbies/routing.py
+++ server/lobbies/routing.py
@@ -4,5 +4,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     # We use re_path() due to limitations in URLRouter.
-    re_path(r"ws/job-status/$", consumers.TravelConsumer.as_asgi()),
+   re_path(r'ws/lobbies/(?P<lobby_session>\w+)/$', consumers.TravelConsumer.as_asgi()),
 ]
