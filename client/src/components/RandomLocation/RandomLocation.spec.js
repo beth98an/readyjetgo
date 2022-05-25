afcfca36a8bf6ec09ec338fd1924ae1fd3d06f03
FarzanaRU00 <Farzana29@hotmail.co.uk>
2022-05-25 23:27:23 +0100

    front-end 48%

--- client/src/components/RandomLocation/RandomLocation.spec.js
+++ client/src/components/RandomLocation/RandomLocation.spec.js
@@ -3,6 +3,12 @@ import RandomLocation from "./RandomLocation";
 
 test("it renders the RandomLocation component", () => {
     renderWithReduxProvider(<RandomLocation />);
-    let btn = screen.getByRole("hiddenbtn");
+    let btn = screen.getByTestId("hiddenbtn");
     expect(btn).toBeInTheDocument();
+  });
+
+  test("has a random btn", () => {
+    renderWithReduxProvider(<RandomLocation />);
+    let btn1 = screen.getByRole("randombtn");
+    expect(btn1).toBeInTheDocument();
   });
