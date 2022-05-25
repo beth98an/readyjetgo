430defc5411f6949c1d20028b8449a04406842c1
beth98an <bke.evans@gmail.com>
2022-05-25 11:01:58 +0100

    results page connected

--- client/src/pages/Results/index.js
+++ client/src/pages/Results/index.js
@@ -3,13 +3,16 @@ import { useSelector } from 'react-redux';
 import Maps from '../../components/Map';
 import "./results.css";
 function Results() {
-  const place='London'
+  const place = useSelector(state => state.destination)
   const dest = place.replace(/\s/g, '%20').toLowerCase()
   console.log(dest)
   return (
+    <>
+      <h1>{place}</h1>
       <div className="mapper" role='map' style={{width:'75%', height:'75%'}}>
           <Maps city={dest}/>
       </div>
+    </>
   )
 }
 
