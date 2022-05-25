e37dcc2a0e7ca7785b9d6084b66e597204af30f3
beth98an <bke.evans@gmail.com>
2022-05-25 11:42:29 +0100

    fixed imageon random page

--- client/src/pages/Results/index.js
+++ client/src/pages/Results/index.js
@@ -7,14 +7,13 @@ import "./results.css";
 function Results() {
   const navigate = useNavigate()
   const place = useSelector(state => state.destination)
-  const dest = place.replace(/\s/g, '%20').toLowerCase()
-  console.log(dest)
+  
   return (
     <>
       <h1>{place}</h1>
       <button onClick={()=>navigate('/home')}>Go Home</button>
       <div className="mapper" role='map' style={{width:'75%', height:'75%'}}>
-          <Maps city={dest}/>
+          <Maps city={place}/>
       </div>
       
     </>
