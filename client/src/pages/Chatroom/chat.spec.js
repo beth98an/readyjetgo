ffa6abdbe9c95a5ea7264474d61ae7126e06f3db
FarzanaRU00 <Farzana29@hotmail.co.uk>
2022-05-26 10:05:33 +0100

    failed test

--- client/src/pages/Chatroom/chat.spec.js
+++ client/src/pages/Chatroom/chat.spec.js
@@ -1,18 +1,22 @@
 import { screen } from "@testing-library/react";
+import { render } from "express/lib/response";
 import Chat from "./index";
+import { sendMessage } from './index'
 
-describe('Chatroom', () => {
-    
+    test('should render a message button', () => {
+        renderWithReduxProvider(<sendMessage />)
+        const sendMessage = screen.getByTestId('sendMessage')
+        expect(sendMessage.textContent).toContain('Send Message!')
+    })
+
     test('has an id of heading"', () => {
         renderWithReduxProvider(<Chat />);
         const heading = screen.getByRole('heading')
         expect(heading.textContent).toContain("Ready Jet Go, Chat!");
     });
 
-test("it renders the Chatroom component", () => {
-    renderWithReduxProvider(<Chat />);
-    let chat = screen.getByRole("chat");
-    expect(chat).toBeInTheDocument();
-  });
-
-})
+    test("it renders the Chatroom component", () => {
+        renderWithReduxProvider(<Chat />);
+        let chat = screen.getByRole("chat");
+        expect(chat).toBeInTheDocument();
+    });
